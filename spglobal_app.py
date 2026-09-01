abb81ca36a9512055ce1dff72d1a38c3202948b8
Michael Doyle <michadoy@cisco.com>
2026-09-01 19:44:12 -0400

    files

---
+++ spglobal_app.py
@@ -0,0 +1,198 @@
+import argparse  # noqa: F401
+import csv  # noqa: F401
+import json  # noqa: F401
+import logging
+import os  # noqa: F401
+import time  # noqa: F401
+from datetime import datetime, timedelta  # noqa: F401
+from pathlib import Path   # noqa: F401
+from typing import TYPE_CHECKING, Any, Dict, List, Union   # noqa: F401
+
+from doyles_sdk.cli.apps._base_app import DoyleApp
+
+try:
+    from . import register_cmd
+except ImportError:
+
+    def register_cmd(cls):
+        return cls
+
+if TYPE_CHECKING:
+    logger: logging.Logger  # just for type checkers
+
+# if you need an API session to a Splunk instance
+# or any access that requires retry and connection pooling
+# uncomment the following lines
+
+from doyles_sdk._wrappers import SplunkSession
+
+_token = "eyJraWQiOiJzcGx1bmsuc2VjcmV0IiwiYWxnIjoiSFM1MTIiLCJ2ZXIiOiJ2MiIsInR0eXAiOiJzdGF0aWMifQ.eyJpc3MiOiJtZG95bGUgZnJvbSBzaC1pLTBjZmI4ZjU2MmEwNWI4ZjQ3Iiwic3ViIjoibWRveWxlIiwiYXVkIjoiY29weSBldmVudHMgZnJvbSBsYXN0Y2hhbmNlaW5kZXMiLCJpZHAiOiJTcGx1bmsiLCJqdGkiOiJhZDQyZGEwYTc1OTc2MjQ3N2RlMzU2MDYzNDEzNzkwNjgyMDA2YzAxNTEyOGIxYzAwN2NmNmM2ZDJmNWRlZGE2IiwiaWF0IjoxNzg3NTg1MTEzLCJleHAiOjE3OTAxNzcxMTMsIm5iciI6MTc4NzU4NTExM30.bySPu5TniTh20tumAzF1dUoOtrwSuNxT22hEPt7N3K-6a_H0sA5WGrTlevUCRyAIx4WnI4x19So7FQsUgo4mFQ"
+session = SplunkSession(token=_token)
+
+testmode = "true"
+
+@register_cmd
+class SpglobalCliApp(DoyleApp):
+    """
+    Auto-generated CLI scaffold for DoyleApp.
+
+    Provides:
+        - argument parsing
+        - logging
+        - threading and multiprocessing support
+
+    Customize this class by overriding:
+        - add_arguments()
+        - args_post_process()
+        - do_* methods
+        - run()
+
+    The following can referenced directly in your methods:
+
+        - __package__
+        - __author__
+        - __email__
+        - __license__
+        - __python__
+        - __platform__
+        - __system__
+        - __machine__
+        - __release__
+        - __mem_total__
+        - __mem_avail__
+        - __cpu_count__
+        - __version__
+    """
+
+    command_name = "spglobal"
+    mp_safe = False       # Allow multiprocessing
+    thread_safe = True   # Allow threads
+
+
+    @classmethod
+    def add_arguments(cls, parser):
+        """
+        # Add your custom CLI arguments here.
+
+        The following arguments are **already defined by the base parser**
+        and should NOT be redefined here:
+
+        --help
+        --debug
+        --verbose
+        --log-dir
+        --version
+        --log-level
+
+        **Example:**
+            parser.add_argument("--example", help="Example argument", default="value")
+        """
+        parser.add_argument("--example", help="Example argument", default="value")
+
+    @classmethod
+    def args_post_process(cls, parser):
+        """
+        # Validate or transform parsed args.
+
+        **Example:**
+            if parser.my_option and not valid(parser.my_option):
+                raise ValueError("Invalid option")
+        """
+        pass
+
+    @staticmethod
+    def do_example_task(arg):
+        """
+        Example do_* method demonstrating concurrency support.
+
+        logger is automatically injected to all do_* methods
+
+        """
+        payload = {
+            "output_mode": "json",
+            "count": 0,
+            "adhoc_search_level": "fast",
+            "allow_partial_results": "false",
+            "earliest_time": "-2mon@mon",
+            "enable_lookups": "false",
+            "exec_mode": "oneshot",
+            "latest_time": "@mon",
+            "reload_macros": "false"}
+        # time.sleep(0.5)
+        data = arg.get("result")
+        if data:
+            indexes = data.get("index")
+            if not isinstance(indexes, list):
+                logger.error("Unknown dest index for event %s", data)
+            else:
+                st = data.get("sourcetype")
+                s = data.get("source")
+                h = data.get("host")
+                idx = [x for x in indexes if x != "lastchanceindex"]
+                logger.debug(idx[0])
+                payload["search"] = f"search index=lastchanceindex sourcetype={json.dumps(st)} source={json.dumps(s)} host={json.dumps(h)} | fields _time, _raw, sourcetype, source, host | collect testmode={testmode} index={idx[0]} output_format=hec"                
+
+                logger.debug(f"Running task with {list(payload.items())}") # noqa: F821
+                
+                response = session.post("https://spglobal.splunkcloud.com:8089/services/search/jobs", data=payload)
+                result = {"status_code": response.status_code, "result": data, "count": len(response.json().get("results"))}
+                if response.status_code in [200, 201]:
+                    logger.notice(result)
+                else:
+                    logger.error(result)
+
+                return result
+
+                # if response.status_code in [200, 201]:
+                #     result = {"status_code": response.status_code, "result": data, "count": len(response.json().get("results"))}
+                #     return result
+                # else:
+                #     result = {"status_code": response.status_code, "message": response.json().get("messages"), "arg": arg}
+                #     logger.error(result)
+                #     return result
+
+
+    def reprocess(self, result: dict) -> list[tuple[callable, object]]:
+        """Retries failed attempts by adding returning the tuple of failed attempts"""
+
+        if result.status_code in [200, 201]:
+            return []
+        else:
+            return([(self.do_example_task, result)])
+
+    def run(self):
+        """
+        # Main application logic.
+        Prefer delegating to do_* methods for consitent behavior using any execution method.
+
+        The following are available:
+
+            - self.logger
+            - self.args
+        """
+        # open the jsonl file and dispatch to the workers
+        self._results_file_path = "./results.jsonl"
+        args_list = list()
+        with open("missing_data_lastchance.jsonl", "r") as f:
+            for line in [line.strip() for line in f]:
+                args_list.append(json.loads(line))
+        # args_list = [self.args.example] if isinstance(self.args.example, str) else self.args.example
+        results = self.run_with_workers(self.do_example_task, args_list, max_workers=25, result_func=self.log_result)
+
+        with open("results.json", "w") as f:
+            f.write(json.dumps(results, indent=2)) 
+
+
+
+# The following is required boilerplate
+# DO NOT MODIFY
+def cli():
+    app = SpglobalCliApp()
+    try:
+        app.run()
+    finally:
+        app.shutdown_logging()
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(cli())
