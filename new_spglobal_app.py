9c0d11bb66b78eb6e354bdf8166e779a9766131f
Michael Doyle <michadoy@cisco.com>
2026-09-06 15:29:03 -0400

    modified:   new_spglobal_app.py

--- new_spglobal_app.py
+++ new_spglobal_app.py
@@ -298,6 +298,8 @@ class NewSpglobalCliApp(DoyleApp):
         # This operation is instantaneous and safe from interruptions
         if testmode == "false":
             os.replace(temp_path, src_file)
+        else:
+            raise SystemExit("testmode")
 
         # args_list = [self.args.example] if isinstance(self.args.example, str) else self.args.example
         results = self.run_with_workers(self.do_example_task, args_list, max_workers=5, result_func=self.log_result)
