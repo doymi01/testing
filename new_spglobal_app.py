554b821c563d20c3d4223c2bed18678b5cb9552d
Michael Doyle <michadoy@cisco.com>
2026-09-11 11:29:18 -0400

    modified:   new_spglobal_app.py

--- new_spglobal_app.py
+++ new_spglobal_app.py
@@ -285,13 +285,13 @@ class NewSpglobalCliApp(DoyleApp):
         if testmode == "false":
             os.replace(temp_path, src_file)
             # done_set is not needed for the generator after the new file is written
-            done_set = set()
+            del done_set
         # results = self.run_with_workers(self.do_example_task, args_list, max_workers=15, result_func=self.log_result)
         # 5. Worker execution memory fix
         # Instead of keeping a giant args_list in memory, pass the generator 
         # directly to the workers if `run_with_workers` accepts iterables.
         # If it REQUIRES a materialized list, read it back from the newly saved disk file!
-        final_worker_args = stream_and_chunk_items(src_file, done_set) 
+        final_worker_args = stream_and_chunk_items(src_file, set()) 
         # OR if your worker framework requires a rigid list:
         # with open(src_file, "r") as f:
         #     final_worker_args = [json.loads(l) for l in f]
